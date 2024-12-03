0445bc0f36c827e14ce5689a7e4df90ee22b1bbb
burcudumanl <153668213+burcudumanl@users.noreply.github.com>
2024-12-03 22:24:21 +0300

    Add files via upload

---
+++ City.java
@@ -0,0 +1,19 @@
+public class City {
+    private String citylabel;
+
+    public City(String citylabel){
+        this.citylabel=citylabel;
+    }
+    public void setCitylabel(String citylabel){
+        this.citylabel=citylabel;
+
+    }
+    public String getCitylabel(){
+        return citylabel;
+    }
+
+
+
+
+
+}
